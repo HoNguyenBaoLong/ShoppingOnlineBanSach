7c01414dfcffeb392b6155b2fb1fd6cd4ec86d3a
HoNguyenBaoLong <128351649+HoNguyenBaoLong@users.noreply.github.com>
2023-11-26 15:10:01 +0700

    upload server

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'shoppingonline.pye7axt.mongodb.net',
+  DB_USER: 'HNBaoLong',
+  DB_PASS: 'Baolong357',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: '442',
+  JWT_EXPIRES: '351636000000', // in milliseconds
+  EMAIL_USER: 'honguyenbaolong6@gmail.com', // gmail service
+  EMAIL_PASS: 'aqtkcskrzjytghrb'
+};
+module.exports = MyConstants;
